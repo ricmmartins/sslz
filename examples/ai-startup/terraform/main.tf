a42b5c2c6c42a005d9eafa405e96f79619ffea4d
Ricardo Martins <44813563+ricmmartins@users.noreply.github.com>
2026-02-22 18:41:11 -0500

    Fix storage HTTPS parity and integration test subscription secrets

--- examples/ai-startup/terraform/main.tf
+++ examples/ai-startup/terraform/main.tf
@@ -303,6 +303,7 @@ resource "azurerm_storage_account" "this" {
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
   min_tls_version                 = "TLS1_2"
+  https_traffic_only_enabled      = true
   allow_nested_items_to_be_public = false
   tags                            = local.tags
 }
